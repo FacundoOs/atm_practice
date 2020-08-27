143564cf24e2a31b7d5267505d2a4daa880e2136
Facundo Osores <facundo@MacBook-Pro-de-Facundo-2.local>
2020-08-28 00:38:02 +0200

    added expire date test

--- lib/account.rb
+++ lib/account.rb
@@ -1,7 +1,15 @@
 class Account
+    STANDARD_VALIDITY_YRS = 5
 
-    attr_accessor: :pin_code, :balance, :account_status, :exp_date, :owner
+    attr_accessor :pin_code, :balance, :account_status, :exp_date, :owner
 
     def initialize (attrs = {})
         @pin_code = rand(1000..9999)
+        @exp_date = set_expire_date
+
     end
+
+    def set_expire_date
+        Date.today.next_year(STANDARD_VALIDITY_YRS).strftime('%m/%y')
+    end
+end
