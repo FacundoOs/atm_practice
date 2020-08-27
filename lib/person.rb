3d76ef7f7764fc59c11e4f4ae5842b991f84b5a6
Facundo Osores <facundo@MacBook-Pro-de-Facundo-2.local>
2020-08-28 00:52:39 +0200

    new person class added and 2 test

---
+++ lib/person.rb
@@ -0,0 +1,16 @@
+class Person
+    attr_accessor :name
+
+    def initialize (attrs = {})
+        @name = set_owner(attrs[:name])
+    end
+    
+    def set_owner(name)
+        
+        name == nil ?  missing_owner : name
+    end
+      
+    def missing_owner
+        raise "A name is required"
+    end
+end
