25416845b00240764aa2ce01d58f45804d323aed
Facundo Osores <facundo@MacBook-Pro-de-Facundo-2.local>
2020-08-27 23:48:41 +0200

    pin testç

--- lib/atm.rb
+++ lib/atm.rb
@@ -5,11 +5,15 @@ class Atm
         @funds = 1000
     end
 
-    def withdraw(amount, account)
+    def withdraw(amount, pin_code, account)
 
         case
         when insufficient_funds_in_account?(amount, account)
             { status: false, message: 'insufficient funds', date: Date.today }
+        when insufficient_funds_in_atm?(amount)
+            { status: false, message: 'insufficient funds in ATM', date: Date.today }
+        when incorrect_pin?(pin_code, account.pin_code)
+            { status: false, message: 'wrong pin', date: Date.today }
         else
           perform_transaction(amount, account)
         end
@@ -17,6 +21,10 @@ class Atm
     
     private
     
+    def insufficient_funds_in_atm?(amount)
+        @funds < amount
+    end
+
     def insufficient_funds_in_account?(amount, account)
         amount > account.balance
     end
@@ -25,5 +33,9 @@ class Atm
         @funds -= amount
         account.balance = account.balance - amount
         { status: true, message: 'success', date: Date.today, amount: amount }
+    end
+
+    def incorrect_pin?(pin_code, actual_pin)
+        pin_code != actual_pin
     end
 end
